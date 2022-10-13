96c220b8b81ad5ff5cd6611d828226e10eb0ceee
Winlex1 <113421468+Winlex1@users.noreply.github.com>
2022-10-13 08:15:16 +0200

    Update Numbers.cs

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._06
 {
@@ -6,7 +6,26 @@ namespace Open_Lab_04._06
     {
         public int[] NoOdds(int[] numbers)
         {
-            throw new NotImplementedException();
+            int x = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i]%2 == 0)
+
+                {
+                    x++;
+                }
+            }
+            int[] numbers2 = new int[x];
+            int y = 0;
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                if (numbers[j] % 2 == 0)
+                {
+                    numbers2[y] += numbers[j];
+                    y++;
+                }
+            }
+            return numbers2;
         }
     }
 }
